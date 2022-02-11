d72c9cd15cae7cd116aaed4bb4a5dd5b41711c4a
Onik Mkrtchyan <onik_mkrtchyan@mail.ru>
2022-02-11 19:28:55 +0400

    some fixes

--- src/sortingAlgorithms/BubbleSort.java
+++ src/sortingAlgorithms/BubbleSort.java
@@ -1,23 +1,33 @@
+
+package sortingAlgorithms;
+
+import java.util.Arrays;
+
 public class BubbleSort {
 
     public static void main(String[] args) {
         int[] arr = {3, 1, 0, -10, 5, 6, 8, -15};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(bubbleSort(arr)));
+    }
 
+    public static int[] bubbleSort(int[] arr){
         for (int i = 0; i < arr.length - 1; i++) {
             boolean isSorted = true;
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if(arr[j] > arr[j+1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    swap(arr,j);
                     isSorted = false;
                 }
             }
             if(isSorted) break;
         }
+        return arr;
+    }
 
-        for(int a : arr) {
-            System.out.println(a);
-        }
+    private static void swap(int[] arr,int j){
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
     }
 }
