dadf8f63fc12d5c3ffd35cb7a76b4ee71795b12d
Onik Mkrtchyan <onik_mkrtchyan@mail.ru>
2022-05-24 15:27:25 +0400

    Added subArray problem

--- src/SubArrays.java
+++ src/SubArrays.java
@@ -2,17 +2,19 @@
 // using recursion
 public class SubArrays {
 
-    // Recursive function to print all possible subarrays
-    // for given array
-    public static void main(String[] args)
-    {
-        int[] arr = { 1, 2, 3 };
+    // Java program to generate all possible subarrays/subArrays
+    // Complexity- O(n^3) */
+    static int[] arr = new int[]{1, 2, 3, 4};
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
         printSubArrays(arr, 0, 0);
 
     }
 
-    static void printSubArrays(int[] arr, int start, int end)
-    {
+    // Recursive function to print all possible subarrays
+    // for given array
+    static void printSubArrays(int[] arr, int start, int end) {
         // Stop if we have reached the end of the array
         if (end == arr.length)
             return;
@@ -28,27 +30,5 @@ public class SubArrays {
             System.out.println(arr[end] + "]");
             printSubArrays(arr, start + 1, end);
         }
-        return;
     }
-
-    // Java program to generate all possible subarrays/subArrays
-// Complexity- O(n^3) */
-        static int arr[] = new int[]{1, 2, 3, 4};
-
-        // Prints all subarrays in arr[0..n-1]
-        static void subArray( int n)
-        {
-            // Pick starting point
-            for (int i=0; i <n; i++)
-            {
-                // Pick ending point
-                for (int j=i; j<n; j++)
-                {
-                    // Print subarray between current starting
-                    // and ending points
-                    for (int k=i; k<=j; k++)
-                        System.out.print(arr[k]+" ");
-                }
-            }
-        }
 }
