c366cf807f7901869477011bddab42c3e6ec84b1
Onik Mkrtchyan <dev.onik.mkrtchyan@gmail.com>
2022-10-02 20:42:54 +0400

    fix name Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-public class CountOfPairsWithGivenSum {
+public class Main {
 
     public static void main(String[] args) {
 //        int[] arr = { 1, 5, 7, -1, 5, 8};
+        System.out.println("hi");
     }
 
 }
