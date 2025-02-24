ae2a4c55a347becd93dae6da60c1c0f520552d85
Onik Mkrtchyan <dev.onik.mkrtchyan@gmail.com>
2025-02-24 22:25:40 +0400

    added subsets function with backtracking to generate all subsets of a set

--- src/backtracking/main.go
+++ src/backtracking/main.go
@@ -1,4 +1,31 @@
 package main
 
+import "fmt"
+
 func main() {
+	set := []int{1, 2, 3}
+	result := subsets(set)
+
+	fmt.Println("Result ", result)
+}
+
+func subsets(set []int) [][]int {
+	res := make([][]int, 0)
+	current := make([]int, 0, len(set))
+	backtracking(&res, current, 0, set)
+	return res
+}
+
+// todo not working
+func backtracking(res *[][]int, current []int, i int, set []int) {
+	if i < len(set) {
+		*res = append(*res, current)
+		return
+	}
+
+	current = append(current, set[i])
+	backtracking(res, current, i+1, set)
+
+	current = current[:len(current)-1]
+	backtracking(res, current, i+1, set)
 }
