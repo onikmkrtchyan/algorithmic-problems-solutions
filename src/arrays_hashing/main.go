9d13cd80bc0476cbbd0313378643cb1ba5317631
Onik Mkrtchyan <dev.onik.mkrtchyan@gmail.com>
2025-02-27 19:46:06 +0400

    add isValidSudoku function to validate Sudoku boards

--- src/arrays_hashing/main.go
+++ src/arrays_hashing/main.go
@@ -118,6 +118,39 @@ func productExceptSelf(nums []int) []int {
 	return res
 }
 
+func isValidSudoku(board [][]byte) bool {
+	rows := make([]map[byte]bool, 9)
+	cols := make([]map[byte]bool, 9)
+	squares := make([]map[byte]bool, 9)
+
+	for i := 0; i < 9; i++ {
+		rows[i] = make(map[byte]bool)
+		cols[i] = make(map[byte]bool)
+		squares[i] = make(map[byte]bool)
+	}
+
+	for r := 0; r < 9; r++ {
+		for c := 0; c < 9; c++ {
+			if board[r][c] == '.' {
+				continue
+			}
+
+			val := board[r][c]
+			squareIdx := (r/3)*3 + c/3
+
+			if rows[r][val] || cols[c][val] || squares[squareIdx][val] {
+				return false
+			}
+
+			rows[r][val] = true
+			cols[c][val] = true
+			squares[squareIdx][val] = true
+		}
+	}
+
+	return true
+}
+
 func main() {
 	//arr2 := []int{1, 1, 1, 2, 2, 2, 3}
 	arr2 := []int{4, 4, 4, 3, 3, 3, 1, 5, 5}
@@ -126,6 +159,20 @@ func main() {
 
 	//print(isAnagram("anagram", "nagaram"))
 
-	fmt.Print(productExceptSelf([]int{1, 2, 3, 4}))
+	//fmt.Print(productExceptSelf([]int{1, 2, 3, 4}))
+
+	board := [][]byte{
+		{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+		{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+		{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+		{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+		{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+		{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
+	}
+
+	fmt.Println(isValidSudoku(board))
 
 }
