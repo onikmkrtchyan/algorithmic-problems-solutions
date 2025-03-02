6d23b0bd43e17c694999a6448ba299d55f0c5ab4
Onik Mkrtchyan <dev.onik.mkrtchyan@gmail.com>
2025-03-02 15:20:46 +0400

    add binary search and recursive binary search functions to main

--- src/binary_search/main.go
+++ src/binary_search/main.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"fmt"
+)
+
+func binarySearch(arr []int, target int) int {
+	l := 0
+	r := len(arr) - 1
+
+	for l <= r {
+		mid := l + (r-l)/2
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] < target {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+
+	return -1
+}
+
+func binarySearchRecursive(arr []int, target, l, r int) int {
+	if l > r {
+		return -1
+	}
+
+	mid := l + (r-l)/2
+
+	if arr[mid] == target {
+		return mid
+	} else if arr[mid] < target {
+		return binarySearchRecursive(arr, target, mid+1, r)
+	} else {
+		return binarySearchRecursive(arr, target, l, mid-1)
+	}
+}
+
+func main() {
+	fmt.Println(binarySearch([]int{1, 2, 3, 4, 5}, 3))
+	fmt.Println(binarySearchRecursive([]int{1, 2, 3, 4, 5}, 3, 0, 4))
+}
