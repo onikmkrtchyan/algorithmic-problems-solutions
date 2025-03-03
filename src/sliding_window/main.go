b5502fa92f504d3aeb6c0b420449aafa96781383
Onik Mkrtchyan <dev.onik.mkrtchyan@gmail.com>
2025-03-03 09:58:02 +0400

    add checkInclusion and maxProfit functions to main

--- src/sliding_window/main.go
+++ src/sliding_window/main.go
@@ -1,11 +1,8 @@
 package main
 
-import "fmt"
-
-func main() {
-	arr := []int{1, 2, 3, 3, 3, 4, 4}
-	fmt.Println(removeDuplicate(arr))
-}
+import (
+	"fmt"
+)
 
 func removeDuplicate(arr []int) []int {
 	seen := make(map[int]bool)
@@ -21,4 +18,57 @@ func removeDuplicate(arr []int) []int {
 	return res
 }
 
-//
+// Permutation in String
+func checkInclusion(s1 string, s2 string) bool {
+	lenS1 := len(s1)
+	lenS2 := len(s2)
+	if lenS1 > lenS2 {
+		return false
+	}
+
+	arr := make([]int, 26)
+	for i := 0; i < lenS1; i++ {
+		arr[s1[i]-'a']++
+	}
+	s1Str := fmt.Sprintf("%v", arr)
+	arr = make([]int, 26)
+
+	for i := 0; i < lenS2; i++ {
+
+		arr[s2[i]-'a']++
+
+		if i >= lenS1 {
+			arr[s2[i-lenS1]-'a']--
+		}
+
+		if s1Str == fmt.Sprintf("%v", arr) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func maxProfit(prices []int) int {
+	l := 0
+	r := 1
+	res := 0
+	for r < len(prices) {
+		if prices[l] > prices[r] {
+			l = r
+		} else {
+			res = max(res, prices[r]-prices[l])
+		}
+		r++
+	}
+
+	return res
+}
+
+func main() {
+	arr := []int{1, 2, 3, 3, 3, 4, 4}
+	fmt.Println(removeDuplicate(arr))
+
+	fmt.Println(checkInclusion("abc", "lecabee"))
+	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
+}
