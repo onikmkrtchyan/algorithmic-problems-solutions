64a7a2c7e3d1dfa7f952826319783a4c11c00b3f
Onik Mkrtchyan <dev.onik.mkrtchyan@gmail.com>
2025-03-09 14:17:28 +0400

    LeetCode Integer to Roman and opposite

--- LeetCode/Solution.java
+++ LeetCode/Solution.java
@@ -1325,8 +1325,6 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(romanToInt("MCMXCIV"));
 
-        System.out.println(intToRoman(1994));
     }
 }
